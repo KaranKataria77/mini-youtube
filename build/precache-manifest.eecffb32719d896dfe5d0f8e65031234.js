db3505a05efe0d92909f4afb973349c187c3a167
KaranKataria77 <70756846+KaranKataria77@users.noreply.github.com>
2021-01-03 11:57:51 +0530

    Add files via upload
    build

---
+++ build/precache-manifest.eecffb32719d896dfe5d0f8e65031234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0cc76f699c1b25203a49bac28ea8ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bcda019c6f0e5d607d2",
+    "url": "/static/css/main.fbd03476.chunk.css"
+  },
+  {
+    "revision": "5cd36d11d7587c0fa6a4",
+    "url": "/static/js/2.e68565cf.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.e68565cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bcda019c6f0e5d607d2",
+    "url": "/static/js/main.37edd611.chunk.js"
+  },
+  {
+    "revision": "275b8771aef09e907c13",
+    "url": "/static/js/runtime-main.08151e47.js"
+  }
+]);
